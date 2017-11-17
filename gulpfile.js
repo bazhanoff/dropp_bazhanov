d0d6b455ae417704baf5164d9c5d10f5cfff33af
Vlad Bazhanov <bazhanvlad58@yandex.ru>
2017-11-17 15:03:43 +0400

    Изменения на сайте: Изменения в первых трёх блоках: 1. В шапке убрана лишняя разметка каркаса 2. Добавлена типографика к заголовкам 3. Убраны лишние свойства 4. В блоке services убраны классы для абзацев Изменения в проекте сайта: 1. Добавлена сборка пакетов с помощью Gulp 2. Стилизация сайта будет основываться на препроцессоре LESS 3. Автоматически добавлены префиксы к свойства с помощью Gulp

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+
+gulp.task('less', function() {
+	return gulp.src('css/**/*.less')
+	.pipe(less())
+	.pipe(gulp.dest('css'))
+});
+
+var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('prefixes', function () {
+    return gulp.src('css/style.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('css'));
+});
