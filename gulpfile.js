ef9ee19e882da09e1167671529f5929f73b892d8
Vlad Bazhanov <bazhanvlad58@yandex.ru>
2017-11-20 15:54:50 +0400

    Изменения на сайте 1. Переделан фон в блоке banner 2. В блоке services преревёрстаны блоки с помощью flexbox 3. Файловая структура css-каталога разбита на папку blocks, в которую помещены блоки по методологии БЭМ 4. Размер шрифта сайта и отступы сделаны адаптивно 5. Блок portfolio перевёрстан с помощью flexbox Итог: 1. Первые 4 блока сайта сделаны, необходимо добавить адаптивность 2. Последние два блока переверстать и добавить адаптивность

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 
 gulp.task('less', function() {
-	return gulp.src('css/**/*.less')
+	return gulp.src('css/style.less')
 	.pipe(less())
 	.pipe(gulp.dest('css'))
 });
