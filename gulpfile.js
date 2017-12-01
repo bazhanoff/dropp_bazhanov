bde78d483a46d9c50f6d7fc198dcbd0602434eb6
Vlad Bazhanov <bazhanvlad58@yandex.ru>
2017-12-01 08:43:30 +0400

    Оптимизация сайта 1. Оптимизированны картинки 2. Минифицированы index.html и style.css в папке build 3. Поправлена вёрстка, убраны лишние классы в блоке services и posts

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var cssmin = require('gulp-cssmin')
+var htmlmin = require('gulp-htmlmin');
 
 gulp.task('less', function() {
 	return gulp.src('css/style.less')
@@ -17,4 +19,19 @@ gulp.task('prefixes', function () {
             cascade: false
         }))
         .pipe(gulp.dest('css'));
+});
+
+gulp.task('cssmin', function () {
+    return gulp.src('css/style.css')
+        .pipe(cssmin())
+        .pipe(gulp.dest('build/css'))
+});
+
+var gulp = require('gulp');
+var htmlmin = require('gulp-htmlmin');
+
+gulp.task('htmlmin', function() {
+  return gulp.src('index.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('build'));
 });
